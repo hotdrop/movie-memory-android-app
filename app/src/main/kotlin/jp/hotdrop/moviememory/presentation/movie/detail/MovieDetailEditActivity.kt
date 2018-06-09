d02c7504f3663bda007affdc99a75cc8f3d2aeb4
kenji <gunsecondstage0106@gmail.com>
2018-06-10 01:18:11 +0900

    カレンダーアイコンをタップした時のDatePickerを実装

--- app/src/main/kotlin/jp/hotdrop/moviememory/presentation/movie/detail/MovieDetailEditActivity.kt
+++ app/src/main/kotlin/jp/hotdrop/moviememory/presentation/movie/detail/MovieDetailEditActivity.kt
@@ -1,5 +1,6 @@
 package jp.hotdrop.moviememory.presentation.movie.detail
 
+import android.app.DatePickerDialog
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
@@ -12,6 +13,8 @@ import jp.hotdrop.moviememory.R
 import jp.hotdrop.moviememory.databinding.ActivityMovieDetailEditBinding
 import jp.hotdrop.moviememory.presentation.BaseActivity
 import kotlinx.android.synthetic.main.activity_movie_detail_edit.*
+import org.threeten.bp.LocalDate
+import timber.log.Timber
 import javax.inject.Inject
 
 class MovieDetailEditActivity: BaseActivity() {
@@ -43,6 +46,10 @@ class MovieDetailEditActivity: BaseActivity() {
             it?.let { binding.movie = it }
         })
 
+        binding.calendarImageIcon.setOnClickListener {
+            showDatePickerDialog()
+        }
+
         viewModel.saveSuccess.observe(this, Observer {
             it?.let {
                 if (it) {
@@ -63,6 +70,22 @@ class MovieDetailEditActivity: BaseActivity() {
         viewModel.loadMovie(movieId)
     }
 
+    private fun showDatePickerDialog() {
+        //val sawDate = binding.movie?.sawDate ?: LocalDate.now()
+        var sawDate = LocalDate.now()
+        if (binding.movieSawDateText.text.isNotEmpty()) {
+            sawDate = LocalDate.parse(binding.movieSawDateText.text)
+        }
+        DatePickerDialog(
+                this,
+                DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                    val selectedDate = LocalDate.of(year, month + 1, dayOfMonth)
+                    Timber.d("選択した日付 = $selectedDate")
+                    binding.movieSawDateText.text = selectedDate.toString()
+                }, sawDate.year, sawDate.monthValue - 1 ,sawDate.dayOfMonth
+        ).show()
+    }
+
     companion object {
         private const val EXTRA_TAG = "EXTRA_TAG"
         fun start(context: Context, movieId: Int) =
@@ -70,5 +93,4 @@ class MovieDetailEditActivity: BaseActivity() {
                     putExtra(EXTRA_TAG, movieId)
                 })
     }
-
 }
