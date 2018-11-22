b93e8a7548d5d86a5dcebb6f89627240da877dbb
kenji <gunsecondstage0106@gmail.com>
2018-11-23 01:30:32 +0900

    feat: 一覧と詳細の画像比率がおかしかったので整えた。また、LeakCanaryが重くてカクカクするので外した

--- app/src/debug/kotlin/jp/hotdrop/moviememory/DebugApp.kt
+++ app/src/debug/kotlin/jp/hotdrop/moviememory/DebugApp.kt
@@ -1,7 +1,6 @@
 package jp.hotdrop.moviememory
 
 import com.facebook.stetho.Stetho
-import com.squareup.leakcanary.LeakCanary
 import timber.log.Timber
 
 class DebugApp: App() {
@@ -9,13 +8,8 @@ class DebugApp: App() {
     override fun onCreate() {
         super.onCreate()
 
-        initLeakCanary()
         initTimber()
         initStetho()
-    }
-
-    private fun initLeakCanary() {
-        LeakCanary.install(this)
     }
 
     private fun initTimber() {
