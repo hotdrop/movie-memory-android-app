3e8d28fcc9640af567bbf58b765fc599c45060b0
kenji <gunsecondstage0106@gmail.com>
2018-11-23 13:17:09 +0900

    fix: ダミーデータ修正

--- app/src/main/kotlin/jp/hotdrop/moviememory/data/remote/DummyApi.kt
+++ app/src/main/kotlin/jp/hotdrop/moviememory/data/remote/DummyApi.kt
@@ -3,6 +3,7 @@ package jp.hotdrop.moviememory.data.remote
 import io.reactivex.Flowable
 import io.reactivex.Single
 import jp.hotdrop.moviememory.data.remote.response.MovieResult
+import jp.hotdrop.moviememory.model.Category
 
 /**
  * TODO これちゃんとAPIできたら削除する
@@ -15,7 +16,12 @@ class DummyApi {
         val data = mutableListOf<MovieResult>()
         data.add(createMovie(id = 1, title = "ファーストコンタクト", overview = "映画その1の情報です", playDate = "2018-11-10"))
         data.add(createMovie(id = 2, title = "セカンドダガシ", overview = "駄菓子のダミー映画です。", playDate = "2018-11-04"))
-        data.add(createMovie(id = 3, title = "サードキューブ", overview = "サードのダミー映画です。", playDate = "2018-10-16"))
+        data.add(createMovie(id = 3, title = "アベンジャーズ・インフィニティーウォー",
+                category = Category(2, "SF・ファンタジー"),
+                overview = "6つ全てを手に入れると、全宇宙の生命の半分を滅ぼす力を得る「インフィニティーストーン」その無限のエネルギーを持つ石を手にせんと迫り来るサノス。\n" +
+                        "彼の野望を阻止するため、最強ヒーロチーム・アベンジャーズが結集し、人類の命運をかけた壮絶なバトルが今、始まる！",
+                playDate = "2018-11-23",
+                imageUrl = ""))
         data.add(createMovie(id = 4, title = "シャークフォース", overview = "ダミーサメが出てくる映画です。", playDate = "2018-10-16"))
         data.add(createMovie(id = 5, title = "ファイブセンス", overview = "碁盤目なだけに光ると五番映画ができます。際がそろそろ消える気がします。", playDate = "2018-10-16"))
         data.add(createMovie(id = 6, title = "シックスエレメンツ", overview = "色々混じったダミー映画であります。", playDate = "2018-07-21"))
@@ -29,18 +35,19 @@ class DummyApi {
         dummyData = data
     }
 
-    fun getMovies(fromId: Int?): Single<List<MovieResult>> =
+    fun nowPlaying(fromId: Int?): Single<List<MovieResult>> =
             fromId?.let {
                 Single.just(dummyData.subList(fromId - 1, dummyData.size - 1))
             } ?: Single.just(dummyData)
 
 
-    fun getComingSoon(index: Int, offset: Int): Flowable<List<MovieResult>> =
+    fun comingSoon(index: Int, offset: Int): Flowable<List<MovieResult>> =
         Flowable.just(dummyData.subList(index, index + offset))
 
     private fun createMovie(
             id: Int,
             title: String? = null,
+            category: Category = Category(1, "アクション"),
             overview: String? = null,
             imageUrl: String? = null,
             playDate: String? = null,
@@ -49,6 +56,8 @@ class DummyApi {
             MovieResult(
                     id = id,
                     title = title ?: "ダミータイトルです。",
+                    categoryId = category.id,
+                    categoryName = category.name,
                     overview = overview ?: "ここには概要を入れます。ある程度長い概要でも表示されるようにちょっと長いものを入れます。\nアベンジャーズ4が楽しみですがその前にキャプテンマーベルがどうなるのか楽しみです。\nそれとエージェントオブヒドラ（エージェントオブシールドのシーズン4の後半）はちょっとないなと思いました。。",
                     imageUrl = imageUrl,
                     playingDate = playDate ?: "2018-11-01",
