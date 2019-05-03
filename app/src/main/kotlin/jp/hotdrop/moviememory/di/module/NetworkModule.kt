11234956df98045394a5709bbec7abd97bbed457
kenji <gunsecondstage0106@gmail.com>
2019-05-03 10:15:39 +0900

    refactor: ライブラリのバージョンアップと未使用のライブラリを削除。また、Stethoがエラーで落ちていたのでNetworkInterceptorを使うよう修正。本当はアノテーションでInterceptorとNetworkInterceptorを分けるべき。やり方忘れたのであとで対応

--- app/src/main/kotlin/jp/hotdrop/moviememory/di/module/NetworkModule.kt
+++ app/src/main/kotlin/jp/hotdrop/moviememory/di/module/NetworkModule.kt
@@ -1,41 +1,25 @@
 package jp.hotdrop.moviememory.di.module
 
-import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
-import jp.hotdrop.moviememory.BuildConfig
-import jp.hotdrop.moviememory.data.remote.AppJsonAdapterFactory
 import jp.hotdrop.moviememory.di.InterceptorModule
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module(includes = [InterceptorModule::class])
 object NetworkModule {
 
-    @JvmStatic @Provides @Singleton
+    @JvmStatic
+    @Provides
+    @Singleton
     fun provideOkHttpClient(interceptors: Set<@JvmSuppressWildcards Interceptor>): OkHttpClient =
             OkHttpClient.Builder()
                     .readTimeout(10, TimeUnit.SECONDS)
                     .connectTimeout(10, TimeUnit.SECONDS)
                     .apply {
-                        interceptors.forEach { addInterceptor(it) }
+                        interceptors.forEach { addNetworkInterceptor(it) }
                     }
                     .build()
-
-    @JvmStatic @Provides @Singleton
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
-        return Retrofit.Builder()
-                .client(okHttpClient)
-                .baseUrl(BuildConfig.API_ENDPOINT)
-                .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder()
-                        .add(AppJsonAdapterFactory.INSTANCE)
-                        .build()))
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build()
-    }
 }
